5c83b55bf36f854283fd2b5cb839df5cc3abb2c8
Nikhil Chaudhary <nikhilchaudhary285@gmail.com>
2023-07-08 11:30:13 +0530

    forum

--- partials/_loginmodal.php
+++ partials/_loginmodal.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="loginmodalLabel">Login to iDiscuss</h1>
+                <h1 class="modal-title fs-5" id="loginmodalLabel">Login to Ask & Tell</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form action="/forum/partials/_handleLogin.php" method="post">
